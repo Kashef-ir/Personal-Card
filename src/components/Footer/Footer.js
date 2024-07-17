7234d4a5ac078d0e2a785f588964b8e6a27fdbbf
Muhamad Hashemi <muhamadhashemi142@gmail.com>
2024-07-17 11:50:32 +0330

    Fix Footer and Add bg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,22 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons/faXTwitter";
 import { faTelegram } from "@fortawesome/free-brands-svg-icons/faTelegram";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
-import { faSlack } from "@fortawesome/free-brands-svg-icons/faSlack";
 
 export default function Footer() {
     return (
         <div className="social">
-            <a href="" className="social-icon hvr-grow">
+            <a href="https://x.com/KashefSaltane" className="social-icon hvr-grow">
                 <FontAwesomeIcon icon={faXTwitter} />
             </a>
-            <a href="" className="social-icon hvr-grow">
+            <a href="https://t.me/cornerperson" className="social-icon hvr-grow">
                 <FontAwesomeIcon icon={faTelegram} />
             </a>
-            <a href="" className="social-icon hvr-grow">
+            <a href="https://github.com/Kashef-ir" className="social-icon hvr-grow">
                 <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a href="" className="social-icon hvr-grow">
-                <FontAwesomeIcon icon={faSlack} />
             </a>
         </div>
     );
