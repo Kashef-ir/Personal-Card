283855da6f643574ad5eff82cd25904edf470bd3
Muhamad Hashemi <muhamadhashemi142@gmail.com>
2024-07-17 21:18:22 +0330

    Fix mobile view

--- src/index.js
+++ src/index.js
@@ -8,3 +8,11 @@ root.render(
         <App></App>
     </div>
 );
+
+function setFullHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  window.addEventListener('load', setFullHeight);
+  window.addEventListener('resize', setFullHeight);
