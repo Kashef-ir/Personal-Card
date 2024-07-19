30a37a5eacc38c8782ef114c3dc943ef180e6a8d
Muhamad Hashemi <muhamadhashemi142@gmail.com>
2024-07-19 09:36:10 +0330

    Fix UI

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -6,8 +6,8 @@ export default function Info() {
         <div>
             <img src={profileImg} alt="Profile" className="profile-img"></img>
             <h1 className="name">Mohammad Hashemi</h1>
-            <h3 className="skill">Front-End Developer</h3>
-            <h4 className="tech">ReactJs | Tailwind</h4>
+            <h3 className="sub-name skill">Front-End Developer</h3>
+            <h4 className="sub-name tech">ReactJs | Tailwind</h4>
         </div>
     )
 }
